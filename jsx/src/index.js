fc93fe23e37633446410d9b6b4b9d279f09dd07f
dkang417 <dkang417@gmail.com>
2018-12-19 20:43:00 -0500

    update jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,15 +4,24 @@ import ReactDOM from 'react-dom';
 
 
 // create a react component
+const App = () => {
 
-const App =  () => {
-    return <div>Hi there!</div>;
+    const buttonText = 'click me';
+    return (
+        <div>
+            <label className="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                { buttonText }
+            </button>
+        </div>
+    );
 };
-
 
 // take the react component and show it on the scree
 ReactDOM.render(
     <App />, 
     document.querySelector('#root')
 );
-
