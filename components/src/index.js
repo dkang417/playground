8b26bd7589ac4771030351557fdfa940836fdb22
dkang417 <dkang417@gmail.com>
2018-12-23 22:07:23 -0500

    update cards

--- components/src/index.js
+++ components/src/index.js
@@ -8,10 +8,33 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />   
-            <CommentDetail author="Sam" timeAgo="Today at 4:35PM" text="Can anyone make it out to my Christmas party???" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex"  timeAgo="Today at 2:00AM" text="I think i can make it to the party" avatar={faker.image.avatar()} />
-            <CommentDetail author="Matthew" timeAgo="Today at 8:00PM" text="i will def come" avatar={faker.image.avatar()} />
+            <ApprovalCard>  
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:35PM"
+                    text="Can anyone make it out to my Christmas party???"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00AM"
+                    text="I think i can make it to the party"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    author="Matthew"
+                    timeAgo="Today at 8:00PM"
+                    text="i will def come"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
         </div>
     );
 
