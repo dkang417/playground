57907f54faca9b3bc4581eb18739cd4ace22b641
dkang417 <dkang417@gmail.com>
2018-12-23 20:35:17 -0500

    update approvalcard

--- components/src/index.js
+++ components/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />   
             <CommentDetail author="Sam" timeAgo="Today at 4:35PM" text="Can anyone make it out to my Christmas party???" avatar={faker.image.avatar()}/>
             <CommentDetail author="Alex"  timeAgo="Today at 2:00AM" text="I think i can make it to the party" avatar={faker.image.avatar()} />
             <CommentDetail author="Matthew" timeAgo="Today at 8:00PM" text="i will def come" avatar={faker.image.avatar()} />
