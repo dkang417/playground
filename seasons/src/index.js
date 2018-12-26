c72a550322e1f0c9b1bbcc53e218ccd5309fff85
dkang417 <dkang417@gmail.com>
2018-12-25 20:30:38 -0500

    update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 
 // class Component not functional Component
 class App extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+        // this is the only time we direct assignment on this.state = something
+        this.state = { lat: 9999 };
+
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                // we call setstate to update our lat position
+                this.setState({ lat: position.coords.latitude });
+            },
             err => console.log(err)
         );
-
-        return <div> Latititude HERE: </div>;
+    }
+    // react says we must define render
+    render() {
+        return <div> Latititude HERE:{this.state.lat} </div>;
     }
 }
 
