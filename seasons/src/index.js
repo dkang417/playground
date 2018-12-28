0542ac56208a541b93bcc3fee2d28a1ab96eae64
dkang417 <dkang417@gmail.com>
2018-12-27 20:14:35 -0500

    update render method with no conditionals

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './spinner';
 
-
 // class Component not functional Component
 class App extends React.Component {
     
@@ -17,8 +16,8 @@ class App extends React.Component {
         );
     }
   
-    // react says we must define render
-    render() {
+    // helper method so we dont have conditionals in render method
+    renderContent() {
 
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
@@ -27,9 +26,18 @@ class App extends React.Component {
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-        return <Spinner />
+
+        return <Spinner message="Please accept location request" />;
+    }
+
+    // react says we must define render
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-
