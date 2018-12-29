893011977335b56a19c52c1ca045603df75dc388
dkang417 <dkang417@gmail.com>
2018-12-29 12:21:42 -0500

    update

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 class SearchBar extends React.Component {
    
-state = { term: ''};
-    onFormSubmit(event) {
+    state = { term: '' };
+    
+    onFormSubmit= (event) => {
         event.preventDefault();
         console.log( this.state.term );    
-}    
+    }    
     render() {
         return (
             <div className="ui segment">  
@@ -17,7 +18,7 @@ state = { term: ''};
                         <input
                             type="text"
                             value={this.state.term}
-                            onChange={e => this.setState({ term: e.target.value.toUpperCase() })}
+                            onChange={e => this.setState({ term: e.target.value })}
                         />
                     </div>
                 </form>
