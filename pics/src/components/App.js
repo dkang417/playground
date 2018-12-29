f7e581b5d5876238e09b7d8b3d5f5d792c4e4656
dkang417 <dkang417@gmail.com>
2018-12-28 20:44:13 -0500

    styling searchbar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar';
 
 
 const App = () => {
-    return <div> <SearchBar /> </div>;
+    return <div className="ui container" style={{ marginTop: '50px'}}> <SearchBar /> </div>;
 };
 
 
