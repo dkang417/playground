c81157c8a3f480ee7167d98a5754dd5fcdcaa464
dkang417 <dkang417@gmail.com>
2019-01-02 20:50:51 -0500

    add search term

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    state = { searchTerm: '' };
+    
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    };
+
     render() {
-        return <div>SearchBar hereee</div>;
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video search </label>
+                        <input
+                            type="text"
+                            value={this.state.searchTerm}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>    
+            </div>
+        );
     }
 }
+
 export default SearchBar;
+
