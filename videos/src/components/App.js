632170a4daa900cbbd049f31e286aef6e1aea2c7
dkang417 <dkang417@gmail.com>
2019-01-03 20:54:14 -0500

    update video list and item

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component{
-    state = { videos: [] };
+    state = {videos: [] };
 
     onTermSubmit = async term => {
         const response =  await youtube.get('/search', {
@@ -12,14 +13,14 @@ class App extends React.Component{
             }
         });
         this.setState({ videos: response.data.items });
-        console.log(this.state.videos);
+        // console.log(this.state.videos);
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos
+                <VideoList videos={this.state.videos} /> 
             </div>
         )
     }
