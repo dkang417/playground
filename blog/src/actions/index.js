631386dbe21efdc027dec79552bef7035bf723ff
dkang417 <dkang417@gmail.com>
2019-01-09 20:16:48 -0500

    update

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,10 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// define function that returns a function
+export const fetchPosts = () => async dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: response })
+      
 };
 
