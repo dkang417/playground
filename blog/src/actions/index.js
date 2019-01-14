e413f3ccec61d4426292d029d6dd80d4b84ee39f
dkang417 <dkang417@gmail.com>
2019-01-13 21:41:12 -0500

    add getPostsAndUsers

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,21 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+//redux invokes dispatches 
+export const fetchPostsAndUsers = () => async (dispatch,getState) => {
+    // console.log('about to fetch posts right now...');
+    // dispatch the result of our fetchpost actioncreator
+    // await tells us to first fetch posts then go on to nextline
+    await dispatch(fetchPosts());
+    // console.log(getState().posts);
+    // console.log('fetched posts!');
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+};
+
+
+
 // define function that returns a function
 export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceholder.get('/posts');
@@ -9,12 +24,19 @@ export const fetchPosts = () => async dispatch => {
       
 };
 
-// memoize solution 
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-};
-const _fetchUser = _.memoize(async(id, dispatch) => {
+
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// memoize solution 
+// export const fetchUser = (id) => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
