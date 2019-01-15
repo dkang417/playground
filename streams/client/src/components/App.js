dbac67aa4088a4648475d7a4513b37b488b380f9
dkang417 <dkang417@gmail.com>
2019-01-14 22:10:32 -0500

    update

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+const PageOne = () => {
+    return <div>PageOne</div>;
+};
+const PageTwo = () => {
+    return <div>Pagetwo</div>;
+};
 
 const App = () => {
-    return <div> App here </div>;
+    return (
+        <div> 
+            <BrowserRouter>
+            <div>    
+                <Route path="/" exact component={PageOne} />  
+                    <Route path="/pagetwo" component={PageTwo} />
+            </div>    
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
