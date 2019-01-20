56d5534131c4a5385514e735e4a89c63ee2da440
dkang417 <dkang417@gmail.com>
2019-01-19 22:06:13 -0500

    update

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,14 +1,16 @@
+import { SIGN_IN, SIGN_OUT } from './types';
+
 // all our action creators
 // we call these once user is logged in or out
 
 export const signIn = () => {
     return {
-        type: 'SIGN_IN'
+        type: SIGN_IN
     };
 };
 
 export const signOut = () => {
     return {
-        type: 'SIGN_OUT'
+        type: SIGN_OUT
     };
 };
--- streams/client/src/reducers/authReducers.js
+++ streams/client/src/reducers/authReducers.js
@@ -1,13 +1,17 @@
+// still using strings here
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
 // do not modify this object
 const INITIAL_STATE = {
     isSignedIn: null
 };
 // default state is the const INITIAL_STATE 
+// actions are signin and signout
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:
             return { ...state, isSignedIn: true };
-        case 'SIGN_OUT':
+        case SIGN_OUT:
             return { ...state, isSignedIn: false };
         default:
             return state;    
