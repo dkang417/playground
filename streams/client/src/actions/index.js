89f7f9036a948a12a7c97696af24f89916ea6e51
dkang417 <dkang417@gmail.com>
2019-01-24 22:37:29 -0500

    update action creators

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,7 +1,15 @@
 // our api
 import streams from '../apis/streams';
 // still using strings here
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
 
 // all our action creators
 // we call these once user is logged in or out
@@ -19,8 +27,31 @@ export const signOut = () => {
     };
 };
 
-
+// restful conventions 
 export const createStream = formValues => async dispatch => {
-    streams.post('/streams', formValues);
+    const response = await streams.post('/streams', formValues);
+
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
 };
 
