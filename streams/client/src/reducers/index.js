1b1d97512f2a48e879bdcc83d5f65d717c25bd79
dkang417 <dkang417@gmail.com>
2019-01-26 12:07:55 -0500

    update

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducers';
+import streamReducer from './streamReducer';
 //adding redux form
 import { reducer as formReducer } from 'redux-form';
 
@@ -7,5 +8,6 @@ import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -9,12 +10,18 @@ import {
 export default (state = {}, action) => {
   
     switch (action.type) {
+        case FETCH_STREAMS:
+            // mapkeys creates object with the key being the id of each stream
+            // ...take big object out of mapkeys and add key values to big object
+            return { ...state, ..._.mapKeys(action.payload, 'id') };    
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload }; 
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload }; 
         case EDIT_STREAM:
-            return { ...state, [action.payload.id]: action.payload };    
+            return { ...state, [action.payload.id]: action.payload }; 
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);    
         default:
             return state;    
     }
